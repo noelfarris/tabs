e56ec2fd1e03843012d3797a3d072efc29c755a1
Noel Farris <noel.farris@gmail.com>
2015-09-14 21:28:25 -0500

    tabhighlight

--- scripts/main.js
+++ scripts/main.js
@@ -13,18 +13,24 @@ function displayRed() {
 	$('.hidden').hide();
 	$('.show').hide();
 	$(tabRed).show();
+	$('.tab').css({'backgroundColor': 'white'})
+	$('.red').css({'backgroundColor': 'red'})
 }
 
 function displayGreen() {
 	$('.hidden').hide();
 	$('.show').hide();
 	$(tabGreen).show();
+	$('.tab').css({'backgroundColor': 'white'})
+	$('.green').css({'backgroundColor': 'green'})
 }
 
 function displayBlue() {
 	$('.hidden').hide();
 	$('.show').hide();
 	$(tabBlue).show();
+	$('.tab').css({'backgroundColor': 'white'})
+	$('.blue').css({'backgroundColor': 'blue'})
 }
 
 $(linkRed).click(displayRed);
